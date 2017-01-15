86ebae1f46682a32ef8b0b8580afb027245c1ecc
JoBoccara <joboc100@hotmail.com>
2017-01-15 01:37:22 +0100

    Rename range1 range2 into leftRange rightRange for code clarity

--- set_seggregate.hpp
+++ set_seggregate.hpp
@@ -3,39 +3,39 @@
 
 #include "metaprog.hpp"
 
-template<class Range1, class Range2,
+template<class LeftRange, class RightRange,
          class OutputItLeft, class OutputItBoth, class OutputItRight, class Compare, class AddToBoth>
-void set_seggregate_impl(Range1 const& range1, Range2 const& range2,
+void set_seggregate_impl(LeftRange const& leftRange, RightRange const& rightRange,
                          OutputItLeft leftOnly, OutputItBoth both, OutputItRight rightOnly,
                          Compare comp, AddToBoth addToBoth)
 {
-    auto it1 = range1.begin();
-    auto it2 = range2.begin();
-    while (it1 != range1.end())
+    auto itLeft = leftRange.begin();
+    auto itRight = rightRange.begin();
+    while (itLeft != leftRange.end())
     {
-        if (it2 == range2.end())
+        if (itRight == rightRange.end())
         {
-            std::copy(it1, range1.end(), leftOnly);
+            std::copy(itLeft, leftRange.end(), leftOnly);
             return;
         }
  
-        if (comp(*it1, *it2))
+        if (comp(*itLeft, *itRight))
         {
-            *leftOnly++ = *it1++;
+            *leftOnly++ = *itLeft++;
         }
         else
         {
-            if (!comp(*it2, *it1))
+            if (!comp(*itRight, *itLeft))
             {
-                *both++ = addToBoth(*it1++, *it2++);
+                *both++ = addToBoth(*itLeft++, *itRight++);
             }
             else
             {
-                *rightOnly++ = *it2++;
+                *rightOnly++ = *itRight++;
             }
         }
     }
-    std::copy(it2, range2.end(), rightOnly);
+    std::copy(itRight, rightRange.end(), rightOnly);
 }
 
 struct AddPairToBoth
@@ -59,45 +59,45 @@ struct AddLeftToBoth
 template<bool bothContainsLeftAndRight>
 struct Set_seggregate_impl
 {
-    template<class Range1, class Range2,
+    template<class LeftRange, class RightRange,
              class OutputItLeft, class OutputItBoth, class OutputItRight, class Compare>
-    void operator()(Range1 const& range1, Range2 const& range2,
+    void operator()(LeftRange const& leftRange, RightRange const& rightRange,
                            OutputItLeft leftOnly, OutputItBoth both, OutputItRight rightOnly,
                            Compare comp)
     {
-        set_seggregate_impl(range1, range2, leftOnly, both, rightOnly, comp, AddPairToBoth());
+        set_seggregate_impl(leftRange, rightRange, leftOnly, both, rightOnly, comp, AddPairToBoth());
     }
 };
 
 template<>
 struct Set_seggregate_impl<false>
 {
-    template<class Range1, class Range2,
+    template<class LeftRange, class RightRange,
              class OutputItLeft, class OutputItBoth, class OutputItRight, class Compare>
-    void operator()(Range1 const& range1, Range2 const& range2,
+    void operator()(LeftRange const& leftRange, RightRange const& rightRange,
                            OutputItLeft leftOnly, OutputItBoth both, OutputItRight rightOnly,
                            Compare comp)
     {
-        set_seggregate_impl(range1, range2, leftOnly, both, rightOnly, comp, AddLeftToBoth());
+        set_seggregate_impl(leftRange, rightRange, leftOnly, both, rightOnly, comp, AddLeftToBoth());
     }
 };
 
 
-template<class Range1, class Range2,
+template<class LeftRange, class RightRange,
          class OutputItLeft, class OutputItBoth, class OutputItRight, class Compare>
-void set_seggregate(Range1 const& range1, Range2 const& range2,
+void set_seggregate(LeftRange const& leftRange, RightRange const& rightRange,
                     OutputItLeft leftOnly, OutputItBoth both, OutputItRight rightOnly,
                     Compare comp)
 {
-    Set_seggregate_impl<BothContainsLeftAndRight<OutputItBoth, Range1, Range2>::value>()(range1, range2, leftOnly, both, rightOnly, comp);
+    Set_seggregate_impl<BothContainsLeftAndRight<OutputItBoth, LeftRange, RightRange>::value>()(leftRange, rightRange, leftOnly, both, rightOnly, comp);
 }
 
-template<class Range1, class Range2,
+template<class LeftRange, class RightRange,
          class OutputItLeft, class OutputItBoth, class OutputItRight>
-void set_seggregate( Range1 const& range1, Range2 const& range2,
-                     OutputItLeft rightOnly, OutputItBoth both, OutputItRight leftOnly)
+void set_seggregate(LeftRange const& leftRange, RightRange const& rightRange,
+                    OutputItLeft rightOnly, OutputItBoth both, OutputItRight leftOnly)
 {
-    set_seggregate(range1, range2, rightOnly, both, leftOnly, std::less<typename std::iterator_traits<typename Range1::const_iterator>::reference>());
+    set_seggregate(leftRange, rightRange, rightOnly, both, leftOnly, std::less<typename std::iterator_traits<typename LeftRange::const_iterator>::reference>());
 }
 
 #endif
