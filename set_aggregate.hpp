23e1220185aef6210060c7d0992783ebbcee019e
JoBoccara <joboc100@hotmail.com>
2017-02-12 02:09:18 +0100

    Add set_aggregate.hpp

---
+++ set_aggregate.hpp
@@ -0,0 +1,24 @@
+#ifndef SET_AGGREGATE_HPP
+#define SET_AGGREGATE_HPP
+
+#include <algorithm>
+#include <iterator>
+#include "set_seggregate.hpp"
+#include <vector>
+
+template<typename Range1, typename Range2, typename OutputIterator, typename Compare, typename Function>
+OutputIterator set_aggregate(Range1 const& range1, Range2 const& range2,
+                   OutputIterator output, Compare compare, Function aggregator)
+{
+    using value_type1 = std::remove_reference_t<decltype(*range1.begin())>;
+    using value_type2 = std::remove_reference_t<decltype(*range2.begin())>;
+
+    std::vector<std::pair<value_type1, value_type2>> elementsInBoth;
+    set_seggregate(range1, range2, output, std::back_inserter(elementsInBoth), output, compare);
+    return std::transform(elementsInBoth.begin(), elementsInBoth.end(),
+                          output,
+                          [aggregator](std::pair<value_type1, value_type2> const& p){return aggregator(p.first, p.second);});
+}
+
+#endif
+
