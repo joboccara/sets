673f795616bb13d260057bba77d847578b03f98f
Jonathan Boccara <boccaraj@gmail.com>
2017-11-14 23:31:03 +0100

    Add set_match algorithm

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "set_aggregate.hpp"
 #include "set_segregate.hpp"
 #include "set_logical_operation.hpp"
+#include "set_match.hpp"
 
 #include <algorithm>
 #include <iostream>
@@ -344,6 +345,42 @@ bool testSetLogicalOperation()
 
     return true;
 }
+    
+bool testSetMatch()
+{
+    struct NumberWithChar
+    {
+        int number;
+        char numberChar;
+        bool operator<(NumberWithChar const& other) const {return number < other.number; }
+        bool operator==(NumberWithChar const& other) const {return number == other.number; }
+    };
+    struct NumberWithString
+    {
+        int number;
+        std::string numberName;
+        bool operator<(NumberWithString const& other) const {return number < other.number; }
+        bool operator==(NumberWithString const& other) const {return number == other.number; }
+    };
+    std::set<NumberWithChar> input1 = {{1,'1'}, {2,'2'}, {3,'3'}, {5,'5'}, {7,'7'}, {8, '8'}};
+    std::set<NumberWithString> input2 = {{2,"two"}, {3,"three"}, {4,"four"}, {5,"five"}, {7,"seven"}, {11,"eleven"}};
+    
+    std::vector<std::pair<NumberWithChar,NumberWithString>> expected = { { {2,'2'}, {2,"two"}   },
+                                                                         { {3,'3'}, {3,"three"} },
+                                                                         { {5,'5'}, {5,"five"}  },
+                                                                         { {7,'7'}, {7,"seven"} } };
+    
+    std::vector<std::pair<NumberWithChar,NumberWithString>> results;
+    struct NumberCharStringCompare
+    {
+        bool operator()(NumberWithChar const& numberWithChar, NumberWithString const& numberWithString) { return numberWithChar.number < numberWithString.number; }
+        bool operator()(NumberWithString const& numberWithString, NumberWithChar const& numberWithChar) { return numberWithString.number < numberWithChar.number; }
+    };
+
+    fluent::set_match(input1, input2, std::back_inserter(results), NumberCharStringCompare());
+    
+    return ranges::equal(results, expected);
+}
 
 bool vectorUnderlyingType()
 {
@@ -452,9 +489,11 @@ void launchTests()
     success &= launchTest("Compare on keys", compareOnKeys);
     success &= launchTest("Compare on keys keep left", compareOnKeysKeepLeft);
 
-    success &= launchTest("Set aggregate", testSetAggregate);
+    success &= launchTest("set_aggregate", testSetAggregate);
     
     success &= launchTest("set_logical_operation", testSetLogicalOperation);
+    
+    success &= launchTest("set_match", testSetMatch);
 
     success &= launchTest("Vector underlying type", vectorUnderlyingType);
     success &= launchTest("Map underlying type", mapUnderlyingType);
---
+++ set_match.hpp
@@ -0,0 +1,26 @@
+//
+//  set_match.hpp
+//  Sets
+//
+//  Created by Jonathan Boccara on 14/11/2017.
+//  Copyright © 2017 Jonathan Boccara. All rights reserved.
+//
+
+#ifndef set_match_h
+#define set_match_h
+
+#include "dead_end_iterator.hpp"
+#include "set_segregate.hpp"
+
+namespace fluent
+{
+
+template<typename Range1, typename Range2, typename OutputIterator, typename Comparator>
+void set_match(Range1 const& range1, Range2 const& range2, OutputIterator out, Comparator comparator)
+{
+    set_segregate(range1, range2, dead_end_iterator(), out, dead_end_iterator(), comparator);
+}
+
+} // namespace fluent
+
+#endif /* set_match_h */
