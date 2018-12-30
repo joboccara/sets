11e34cba1031e283355ba608ed5e0a24160d6ee2
Jonathan Boccara <boccaraj@gmail.com>
2018-12-30 16:56:51 +0100

    Fix typo in markdown formatting

---
+++ Sets/Sets/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sets
+//
+//  Created by Jonathan Boccara on 10/11/2017.
+//  Copyright © 2017 Jonathan Boccara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
