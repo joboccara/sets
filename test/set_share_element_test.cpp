1c84d03d045e8c61acd56e2d298711b1451e184b
Jonathan Boccara <boccaraj@gmail.com>
2019-01-01 05:34:32 +0100

    Add test to set_share_element

--- test/set_share_element_test.cpp
+++ test/set_share_element_test.cpp
@@ -7,10 +7,15 @@
 TEST_CASE("set_share_element")
 {
     REQUIRE(set_share_element(std::set<int>{2, 3, 4}, std::set<int>{1, 3, 5}));
+    REQUIRE(set_share_element(std::set<int>{1, 3, 5}, std::set<int>{2, 3, 4}));
     REQUIRE(!set_share_element(std::set<int>{2, 3, 4}, std::set<int>{1, 5, 7}));
+    REQUIRE(!set_share_element(std::set<int>{1, 5, 7}, std::set<int>{2, 3, 4}));
     REQUIRE(set_share_element(std::set<int>{2, 3, 4}, std::set<int>{2, 3}));
+    REQUIRE(set_share_element(std::set<int>{2, 3}, std::set<int>{2, 3, 4}));
     REQUIRE(set_share_element(std::set<int>{2, 3, 4}, std::set<int>{4, 5}));
+    REQUIRE(set_share_element(std::set<int>{4, 5}, std::set<int>{2, 3, 4}));
     REQUIRE(set_share_element(std::set<int>{2, 3, 4}, std::set<int>{1, 2}));
+    REQUIRE(set_share_element(std::set<int>{1, 2}, std::set<int>{2, 3, 4}));
     REQUIRE(!set_share_element(std::set<int>{2, 3, 4}, std::set<int>{}));
     REQUIRE(!set_share_element(std::set<int>{}, std::set<int>{2, 3, 4}));
     REQUIRE(!set_share_element(std::set<int>{}, std::set<int>{}));
