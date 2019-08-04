2e8e35d2e562ea50c107cbe65cd02457164a7fbb
Jonathan Boccara <boccaraj@gmail.com>
2019-08-04 10:36:05 +0200

    Fix typo

--- include/detail/algorithm.hpp
+++ include/detail/algorithm.hpp
@@ -7,7 +7,7 @@ namespace ranges
 {
     
 template <typename Range>
-auto raangeSize(Range const& range) -> decltype(std::distance(begin(range), end(range)))
+auto rangeSize(Range const& range) -> decltype(std::distance(begin(range), end(range)))
 {
     return std::distance(begin(range), end(range));
 }
@@ -15,7 +15,7 @@ auto raangeSize(Range const& range) -> decltype(std::distance(begin(range), end(
 template <typename Range1, typename Range2>
 bool equal(Range1 const& range1, Range2 const& range2)
 {
-    if (size(range1) != size(range2)) return false;
+    if (rangeSize(range1) != rangeSize(range2)) return false;
     
     return std::equal(range1.begin(), range1.end(), range2.begin());
 }
@@ -23,7 +23,7 @@ bool equal(Range1 const& range1, Range2 const& range2)
 template <typename Range1, typename Range2>
 bool is_permutation(Range1 const& range1, Range2 const& range2)
 {
-    if (size(range1) != size(range2)) return false;
+    if (rangeSize(range1) != rangeSize(range2)) return false;
     
     return std::is_permutation(range1.begin(), range1.end(), range2.begin());
 }
