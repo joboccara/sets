238cd2a3e8f65b53b6fab9fa2c7494d5f0543be2
Jonathan Boccara <boccaraj@gmail.com>
2019-08-06 05:57:47 +0200

    Integrate forwarding references in set_seggregate

--- include/set_segregate.hpp
+++ include/set_segregate.hpp
@@ -1,38 +1,40 @@
 #ifndef SET_SEGREGATE_HPP
 #define SET_SEGREGATE_HPP
 
+#include "FWD.hpp"
 #include "metaprog.hpp"
+#include <type_traits>
 
 namespace fluent
 {
 namespace details
 {
     
-template<class LeftRange, class RightRange,
-         class OutputItLeft, class OutputItBoth, class OutputItRight, class Compare, class AddToBoth>
-void set_segregate_impl(LeftRange const& leftRange, RightRange const& rightRange,
-                         OutputItLeft leftOnly, OutputItBoth both, OutputItRight rightOnly,
+template<class SetA, class SetB,
+         class OutputOnlyA, class OutputBoth, class OutputOnlyB, class Compare, class AddToBoth>
+void set_segregate_impl(SetA&& setA, SetB&& setB,
+                         OutputOnlyA&& onlyA, OutputBoth&& both, OutputOnlyB&& onlyB,
                          Compare comp, AddToBoth addToBoth)
 {
-    auto left = leftRange.begin();
-    auto right = rightRange.begin();
-    while (left != leftRange.end() && right != rightRange.end())
+    auto xA = setA.begin();
+    auto xB = setB.begin();
+    while (xA != setA.end() && xB != setB.end())
     {
-        if (comp(*left, *right))
+        if (comp(*xA, *xB))
         {
-            *leftOnly++ = *left++;
+            *onlyA++ = *xA++;
         }
-        else if (comp(*right, *left))
+        else if (comp(*xB, *xA))
         {
-            *rightOnly++ = *right++;
+            *onlyB++ = *xB++;
         }
         else
         {
-            *both++ = addToBoth(*left++, *right++);
+            *both++ = addToBoth(*xA++, *xB++);
         }
     }
-    std::copy(left, end(leftRange), leftOnly);
-    std::copy(right, end(rightRange), rightOnly);
+    std::copy(xA, end(setA), onlyA);
+    std::copy(xB, end(setB), onlyB);
 }
 
 struct AddPairToBoth
@@ -56,45 +58,47 @@ struct AddLeftToBoth
 template<bool bothContainsLeftAndRight>
 struct Set_segregate_impl
 {
-    template<class LeftRange, class RightRange,
-             class OutputItLeft, class OutputItBoth, class OutputItRight, class Compare>
-    void operator()(LeftRange const& leftRange, RightRange const& rightRange,
-                           OutputItLeft leftOnly, OutputItBoth both, OutputItRight rightOnly,
-                           Compare comp)
+    template<class SetA, class SetB,
+    class OutputOnlyA, class OutputBoth, class OutputOnlyB, class Compare>
+    static void _(SetA&& setA, SetB&& setB,
+                  OutputOnlyA&& onlyA, OutputBoth&& both, OutputOnlyB&& onlyB,
+                  Compare comp)
     {
-        set_segregate_impl(leftRange, rightRange, leftOnly, both, rightOnly, comp, AddPairToBoth());
+        set_segregate_impl(FWD(setA), FWD(setB), FWD(onlyA), FWD(both), FWD(onlyB), comp, AddPairToBoth());
     }
 };
 
 template<>
 struct Set_segregate_impl<false>
 {
-    template<class LeftRange, class RightRange,
-             class OutputItLeft, class OutputItBoth, class OutputItRight, class Compare>
-    void operator()(LeftRange const& leftRange, RightRange const& rightRange,
-                           OutputItLeft leftOnly, OutputItBoth both, OutputItRight rightOnly,
-                           Compare comp)
+    template<class SetA, class SetB,
+    class OutputOnlyA, class OutputBoth, class OutputOnlyB, class Compare>
+    static void _(SetA&& setA, SetB&& setB,
+                  OutputOnlyA&& onlyA, OutputBoth&& both, OutputOnlyB&& onlyB,
+                  Compare comp)
     {
-        set_segregate_impl(leftRange, rightRange, leftOnly, both, rightOnly, comp, AddLeftToBoth());
+        set_segregate_impl(FWD(setA), FWD(setB), FWD(onlyA), FWD(both), FWD(onlyB), comp, AddLeftToBoth());
     }
 };
 } // namespace details
 
-template<class LeftRange, class RightRange,
-         class OutputItLeft, class OutputItBoth, class OutputItRight, class Compare>
-void set_segregate(LeftRange const& leftRange, RightRange const& rightRange,
-                    OutputItLeft leftOnly, OutputItBoth both, OutputItRight rightOnly,
-                    Compare comp)
+template<class SetA, class SetB,
+class OutputOnlyA, class OutputBoth, class OutputOnlyB, class Compare>
+void set_segregate(SetA&& setA, SetB&& setB,
+                   OutputOnlyA&& onlyA, OutputBoth&& both, OutputOnlyB&& onlyB,
+                   Compare comp)
 {
-    details::Set_segregate_impl<BothContainsLeftAndRight<OutputItBoth, LeftRange, RightRange>::value>()(leftRange, rightRange, leftOnly, both, rightOnly, comp);
+    details::Set_segregate_impl<BothContainsLeftAndRight<typename std::remove_reference<OutputBoth>::type,
+                                                         typename std::remove_reference<SetA>::type,
+                                                         typename std::remove_reference<SetB>::type>::value>::_(FWD(setA), FWD(setB), FWD(onlyA), FWD(both), FWD(onlyB), comp);
 }
 
-template<class LeftRange, class RightRange,
-         class OutputItLeft, class OutputItBoth, class OutputItRight>
-void set_segregate(LeftRange const& leftRange, RightRange const& rightRange,
-                    OutputItLeft leftOnly, OutputItBoth both, OutputItRight rightOnly)
+template<class SetA, class SetB,
+class OutputOnlyA, class OutputBoth, class OutputOnlyB>
+void set_segregate(SetA&& setA, SetB&& setB,
+                   OutputOnlyA&& onlyA, OutputBoth&& both, OutputOnlyB&& onlyB)
 {
-    set_segregate(leftRange, rightRange, leftOnly, both, rightOnly, std::less<typename std::iterator_traits<typename LeftRange::const_iterator>::reference>());
+    set_segregate(FWD(setA), FWD(setB), FWD(onlyA), FWD(both), FWD(onlyB), std::less<typename std::iterator_traits<typename std::remove_reference<SetA>::type::const_iterator>::reference>());
 }
 
 } // namespace fluent
